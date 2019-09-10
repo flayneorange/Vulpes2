d2dd87e284bbd46053e015d074c0bbcb2e8326f5
Joanna Duda <joannaduda1996@gmail.com>
2019-09-10 19:39:49 -0400

    More foxlib dynamic stuff. fixed main arguments being backwards.

--- Vulpes.cpp
+++ Vulpes.cpp
@@ -25,8 +25,10 @@ typedef u64 fuint;
 
 #define fox_interpret_cast(type, expression) (*(type*)&(expression))
 
+#define internal static
+
 template<typename Type>
-void zero(Type* value) {
+internal void zero(Type* value) {
 	memset(value, 0, sizeof(Type));
 }
 
@@ -68,30 +70,15 @@ struct Array {
 	}
 };
 
-template<typename ElementType>
-bool equals(const Array<ElementType>& array0, const Array<ElementType>& array1) {
-	auto length = array0.length;
-	if (array1.length == length) {
-		fox_for (ielement, array1.length) {
-			if (array0[ielement] != array1[ielement]) {
-				return false;
-			}
-		}
-		return true;
+struct String : Array<char> {
+	String() = default;
+	String(const String& other) = default;
+	template<fuint c_string_length> String(char (&c_string)[c_string_length]) {
+		data = c_string;
+		//Good god C++ why???
+		length = c_string_length - 1;
 	}
-	return false;
-}
-
-template<typename ElementType>
-bool operator==(Array<ElementType> array0, Array<ElementType> array1) {
-	return equals(array0, array1);
-}
-
-template<typename ElementType>
-bool operator!=(Array<ElementType> array0, Array<ElementType> array1) {
-	return !equals(array0, array1);
-}
-
+};
 
 struct ConstString : Array<const char> {
 	ConstString() = default;
@@ -101,13 +88,36 @@ struct ConstString : Array<const char> {
 		//Good god C++ why???
 		length = c_string_length - 1;
 	}
+	
+	ConstString(const String& other) {
+		data = other.data;
+		length = other.length;
+	}
 };
 
-void print(ConstString message) {
+template<typename ElementType>
+internal bool equals(const ElementType* data0, fuint length0, const ElementType* data1, fuint length1) {
+	if (length0 == length1) {
+		return memcmp(data0, data1, length0 * sizeof(ElementType)) == 0;
+	}
+	return false;
+}
+
+template<typename ElementType0, typename ElementType1>
+internal bool operator==(Array<ElementType0> array0, Array<ElementType1> array1) {
+	return equals(array0.data, array0.length, array1.data, array1.length);
+}
+
+template<typename ElementType0, typename ElementType1>
+internal bool operator!=(Array<ElementType0> array0, Array<ElementType1> array1) {
+	return !equals(array0.data, array0.length, array1.data, array1.length);
+}
+
+internal void print(ConstString message) {
 	std::cout.write(message.data, message.length);
 }
 
-void test_arrays() {
+internal void test_arrays() {
 	ConstString c_string_conversion_test = "hi";
 	fox_assert(c_string_conversion_test[0] == 'h');
 	fox_assert(c_string_conversion_test[1] == 'i');
@@ -115,8 +125,7 @@ void test_arrays() {
 }
 
 //---Allocators
-#define Static
-fuint align_offset_up(fuint offset, fuint align) {
+internal fuint align_offset_up(fuint offset, fuint align) {
 	fox_assert(align);
 	return offset + ((~offset + 1) & (align - 1));
 }
@@ -131,16 +140,16 @@ struct LinearAllocatorRestorePoint {
 	u8* old_cursor;
 };
 
-void initialize(LinearAllocator* allocator, void* memory, fuint size) {
+internal void initialize(LinearAllocator* allocator, void* memory, fuint size) {
 	allocator->start = allocator->cursor = (u8*)memory;
 	allocator->end = (u8*)memory + size;
 }
 
-void initialize(LinearAllocator* allocator, Array<u8> memory) {
+internal void initialize(LinearAllocator* allocator, Array<u8> memory) {
 	initialize(allocator, memory.data, memory.length);
 }
 
-void* allocate_block(LinearAllocator* allocator, fuint size, fuint align) {
+internal void* allocate_block(LinearAllocator* allocator, fuint size, fuint align) {
 	fox_assert(align);
 	
 	allocator->cursor = (u8*)align_offset_up((fuint)allocator->cursor, align);
@@ -150,7 +159,7 @@ void* allocate_block(LinearAllocator* allocator, fuint size, fuint align) {
 	return result;
 }
 
-void* reallocate_block(LinearAllocator* allocator, void* old_block, fuint old_size, fuint new_size, fuint align) {
+internal void* reallocate_block(LinearAllocator* allocator, void* old_block, fuint old_size, fuint new_size, fuint align) {
 	fox_assert(align);
 	fox_assert(new_size >= old_size);
 	
@@ -162,27 +171,29 @@ void* reallocate_block(LinearAllocator* allocator, void* old_block, fuint old_si
 		return old_block;
 	}
 	
-	return allocate_block(allocator, new_size, align);
+	auto new_block = allocate_block(allocator, new_size, align);
+	memcpy(new_block, old_block, old_size);
+	return new_block;
 }
 
-LinearAllocatorRestorePoint create_restore_point(LinearAllocator* allocator) {
+internal LinearAllocatorRestorePoint create_restore_point(LinearAllocator* allocator) {
 	LinearAllocatorRestorePoint restore_point;
 	zero(&restore_point);
 	restore_point.old_cursor = allocator->cursor;
 	return restore_point;
 }
 
-void restore(LinearAllocator* allocator, LinearAllocatorRestorePoint restore_point) {
+internal void restore(LinearAllocator* allocator, LinearAllocatorRestorePoint restore_point) {
 	allocator->cursor = restore_point.old_cursor;
 }
 
 template<typename Type, typename AllocatorType>
-Type* allocate(AllocatorType* allocator) {
+internal Type* allocate(AllocatorType* allocator) {
 	return (Type*)allocate_block(allocator, sizeof(Type), alignof(Type));
 }
 
 template<typename Type, typename AllocatorType>
-Array<Type> allocate_array(fuint length, AllocatorType* allocator) {
+internal Array<Type> allocate_array(fuint length, AllocatorType* allocator) {
 	Array<Type> new_array;
 	zero(&new_array);
 	new_array.data = (Type*)allocate_block(allocator, sizeof(Type) * length, alignof(Type));
@@ -190,42 +201,120 @@ Array<Type> allocate_array(fuint length, AllocatorType* allocator) {
 	return new_array;
 }
 
-void test_allocators() {
+internal void test_allocators() {
 	//Test initialization, we aren't using an actual buffer here because it doesn't matter
 	constexpr fuint memory_size = 1024;
+	u8 memory_buffer[memory_size];
+	
 	LinearAllocator test_allocator;
-	initialize(&test_allocator, nullptr, memory_size);
-	fox_assert(test_allocator.start == nullptr);
+	initialize(&test_allocator, memory_buffer, memory_size);
+	fox_assert(test_allocator.start == memory_buffer);
 	fox_assert(test_allocator.start == test_allocator.cursor);
-	fox_assert(test_allocator.end == (u8*)memory_size);
+	fox_assert(test_allocator.end == memory_buffer + memory_size);
 	
 	//Test allocation
 	auto test_restore = create_restore_point(&test_allocator);
 	allocate<u32>(&test_allocator);
-	fox_assert(test_allocator.cursor == (void*)4);
+	fox_assert(test_allocator.cursor == memory_buffer + 4);
 	allocate<u8>(&test_allocator);
-	fox_assert(test_allocator.cursor == (void*)5);
+	fox_assert(test_allocator.cursor == memory_buffer + 5);
 	allocate<u16>(&test_allocator);
-	fox_assert(test_allocator.cursor == (void*)8);
+	fox_assert(test_allocator.cursor == memory_buffer + 8);
 	allocate_array<u64>(3, &test_allocator);
-	fox_assert(test_allocator.cursor == (void*)32);
+	fox_assert(test_allocator.cursor == memory_buffer + 32);
 	
 	//Test restore
 	restore(&test_allocator, test_restore);
 	
 	//Test reallocation
-	fox_assert(test_allocator.cursor == nullptr);
+	fox_assert(test_allocator.cursor == memory_buffer);
 	auto allocated_memory = allocate<u16>(&test_allocator);
+	*allocated_memory = 0xdead;
 	auto reallocated_memory = reallocate_block(&test_allocator, allocated_memory, 2, 4, 2);
 	fox_assert(allocated_memory == reallocated_memory);
+	fox_assert(*allocated_memory == 0xdead);
 	
 	restore(&test_allocator, test_restore);
 	allocated_memory = allocate<u16>(&test_allocator);
 	allocate<u16>(&test_allocator);
 	reallocated_memory = reallocate_block(&test_allocator, allocated_memory, 2, 4, 2);
 	fox_assert(allocated_memory != reallocated_memory);
-	fox_assert(reallocated_memory == (void*)4);
-	fox_assert(test_allocator.cursor == (void*)8);
+	fox_assert(*allocated_memory == 0xdead);
+	fox_assert(reallocated_memory == memory_buffer + 4);
+	fox_assert(test_allocator.cursor == memory_buffer + 8);
+}
+
+//---Dynamic Arrays
+template<typename ElementType, typename AllocatorType>
+internal void expand(Array<ElementType>* array, fuint count, AllocatorType* allocator) {
+	auto old_length = array->length;
+	auto new_length = old_length + count;
+	array->data = (ElementType*)reallocate_block(allocator, array->data, old_length * sizeof(ElementType), new_length * sizeof(ElementType), alignof(ElementType));
+	array->length = new_length;
+}
+
+template<typename ArrayType, typename AllocatorType = void>
+internal typename ArrayType::ElementTypeMember& push(ArrayType* array, typename ArrayType::ElementTypeMember new_element, AllocatorType* allocator = nullptr) {
+	auto new_element_index = array->length;
+	expand(array, 1, allocator);
+	return array->get(new_element_index) = new_element;
+}
+
+template<typename ArrayType, typename AllocatorType = void>
+internal void push_array(ArrayType* array, const typename ArrayType::ElementTypeMember* new_elements_data, fuint new_elements_length, AllocatorType* allocator = nullptr) {
+	auto new_element_start_index = array->length;
+	expand(array, new_elements_length, allocator);
+	memcpy(array->data + new_element_start_index, new_elements_data, new_elements_length * sizeof(ArrayType::ElementTypeMember));
+}
+
+template<typename DestinationArrayType, typename SourceArrayType, typename AllocatorType = void>
+internal void push_array(DestinationArrayType* array, SourceArrayType new_elements, AllocatorType* allocator = nullptr) {
+	push_array(array, new_elements.data, new_elements.length, allocator);
+}
+
+template<typename ArrayType, fuint c_string_length, typename AllocatorType = void>
+internal void push_array(ArrayType* array, const char (&c_string)[c_string_length], AllocatorType* allocator = nullptr) {
+	push_array(array, c_string, c_string_length - 1, allocator);
+}
+
+internal void test_dynamic_arrays() {
+	u8 test_buffer[1024];
+	LinearAllocator test_allocator;
+	initialize(&test_allocator, test_buffer, 1024);
+	
+	//Test basic dynamicness
+	Array<u8> test_array0;
+	zero(&test_array0);
+	push(&test_array0, 0, &test_allocator);
+	fox_assert(test_array0.data == test_buffer);
+	fox_assert(test_array0.length == 1);
+	fox_assert(test_array0[0] == 0);
+	push(&test_array0, 1, &test_allocator);
+	fox_assert(test_array0.data == test_buffer);
+	fox_assert(test_array0.length == 2);
+	fox_assert(test_array0[1] == 1);
+	
+	//Make sure push_array works
+	Array<u8> test_array1;
+	zero(&test_array1);
+	push_array(&test_array1, test_array0, &test_allocator);
+	fox_for (i, 2) {
+		fox_assert(test_array1[i] == i);
+	}
+	
+	//Make sure reallocation moves things as necessary
+	push(&test_array0, 2, &test_allocator);
+	fox_assert(test_array0.data != test_buffer);
+	fox_assert(test_array0.length == 3);
+	fox_for (i, 3) {
+		fox_assert(test_array0[i] == i);
+	}
+	
+	//Make sure the same functions work on strings
+	String hello_seven;
+	push_array(&hello_seven, "hello", &test_allocator);
+	push_array(&hello_seven, "7", &test_allocator);
+	fox_assert(hello_seven == ConstString("hello7"));
 }
 
 //---Files
@@ -237,7 +326,7 @@ enum class FileStatus {
 
 //TODO: more detailed error messaging
 template<typename AllocatorType>
-FileStatus read_whole_file(const char* path, AllocatorType* allocator, Array<u8>* out_buffer) {
+internal FileStatus read_entire_file(const char* path, AllocatorType* allocator, Array<u8>* out_buffer) {
 	auto* file = fopen(path, "r+");
 	if (file) {
 		fseek(file, 0, SEEK_END);
@@ -256,47 +345,48 @@ FileStatus read_whole_file(const char* path, AllocatorType* allocator, Array<u8>
 	return FileStatus::error;
 }
 
-void test_file_io() {
+internal void test_file_io() {
 	u8 buffer[128];
 	LinearAllocator test_allocator;
 	initialize(&test_allocator, Array<u8>(buffer));
 	Array<u8> file_contents;
 	zero(&file_contents);
-	auto status = read_whole_file("FoxLibTestFile", &test_allocator, &file_contents);
+	auto status = read_entire_file("FoxLibTestFile", &test_allocator, &file_contents);
 	fox_assert(status == FileStatus::read);
 	ConstString file_contents_string;
 	file_contents_string.data = (const char*)file_contents.data;
 	file_contents_string.length = file_contents.length;
 	fox_assert(file_contents_string == ConstString("hewwo"));
 	zero(&file_contents);
-	status = read_whole_file("FoxLibTestEmptyFile", &test_allocator, &file_contents);
+	status = read_entire_file("FoxLibTestEmptyFile", &test_allocator, &file_contents);
 	fox_assert(status == FileStatus::empty);
 	fox_assert(!file_contents);
-	status = read_whole_file("File name that definitely does not exist", &test_allocator, &file_contents);
+	status = read_entire_file("File name that definitely does not exist", &test_allocator, &file_contents);
 	fox_assert(status == FileStatus::error);
 }
 
-void test_foxlib() {
+internal void test_foxlib() {
 	//library tests
 	test_arrays();
 	test_allocators();
+	test_dynamic_arrays();
 	test_file_io();
 }
 
 //---Vulpes
 LinearAllocator heap_stack;
 
-int main(char** arguments, int argument_count) {
+int main(int argument_count, char** arguments) {
 	test_foxlib();
 	
 	u64 memory_size = mebibytes(1);
 	initialize(&heap_stack, malloc(memory_size), memory_size);
 	
 	if (argument_count > 1) {
-		auto file_path = arguments[0];
+		auto file_path = arguments[1];
 		Array<u8> file_data;
 		zero(&file_data);
-		auto file_read_status = read_whole_file(file_path, &heap_stack, &file_data);
+		auto file_read_status = read_entire_file(file_path, &heap_stack, &file_data);
 		switch (file_read_status) {
 			case FileStatus::read: {
 				print(fox_interpret_cast(ConstString, file_data));
